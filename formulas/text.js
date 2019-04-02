cb8ad1dfa3408eaba2617780b56fbd8c2a95ace9
GongYu <72engineers@gmail.com>
2019-04-02 12:09:53 -0400

    text function updated

--- formulas/text.js
+++ formulas/text.js
@@ -1,86 +1,208 @@
+const FormulaError = require('./error');
+const {FormulaHelpers, Types} = require('./helpers');
+const H = FormulaHelpers;
 
 const TextFunctions = {
-    ASC: (...params) => {
+    //For Double-byte character set (DBCS) languages,
+    // the function changes full-width (double-byte) characters to half-width (single-byte) characters.
 
+    ASC: (text) => {
+        text = H.accept(text, [Types.STRING]);
+        let result = text.replace(
+            /[\uff01-\uff5e]/g,
+            function (ch) {
+                return String.fromCharCode(ch.charCodeAt(0) - 0xfee0);
+            }
+        );
+        return result;
     },
 
-    BAHTTEXT: (...params) => {
-
+    BAHTTEXT: (number) => {
+        // Converts a number to Thai text and adds a suffix of "Baht."
+        // we do not need this function for our project
+        throw FormulaError.NOT_IMPLEMENTED("BAHTTEXT")
     },
 
-    CHAR: (...params) => {
+    CHAR: (number) => {
+        number = H.accept(number, [Types.NUMBER]);
 
+        return String.fromCharCode(number);
     },
 
-    CLEAN: (...params) => {
+    CLEAN: (text) => {
+        text = H.accept(text, [Types.STRING]);
 
+        return text.toString().replace(/[\x00-\x1F]/g, '');
     },
 
-    CODE: (...params) => {
+    CODE: (text) => {
+        text = H.accept(text, [Types.STRING]);
 
+        return text.charCodeAt(0);
     },
 
     CONCAT: (...params) => {
-
+        let str = "";
+        for (let i = 0; i < params.length; i++) {
+            str += params[i];
+        }
+        return str;
     },
 
     CONCATENATE: (...params) => {
-
+        // from Microsoft Documents
+        // Important: In Excel 2016, Excel Mobile, and Excel Online, this function has been replaced with the CONCAT function.
+        // Although the CONCATENATE function is still available for backward compatibility,
+        // you should consider using CONCAT from now on. This is because CONCATENATE may not be available in future versions of Excel.
     },
 
     DBCS: (...params) => {
 
     },
 
-    DOLLAR: (...params) => {
+    DOLLAR: (number) => {
+        number = H.accept(number, [Types.NUMBER]);
+        let str = "";
+
+        while (number > 1000) {
+            str = number % 1000 + "," + str;
+            number = Math.floor(number / 1000);
+            console.log(number, str);
+
+            if (number < 1000) {
+                str = "$" + number + "," + str;
+            }
+        }
+        str = str.slice(0, -1);
+
+        return str;
+    },
+
+    EXACT: (text1, text2) => {
+        text1 = H.accept(text1, [Types.STRING]);
+        text2 = H.accept(text2, [Types.STRING]);
+
+        return text1 === text2;
+    },
+
+    FIND: (find_text, within_text) => {
+        find_text = H.accept(find_text, [Types.STRING]);
+        within_text = H.accept(within_text, [Types.STRING]);
+
+        if (find_text === "") {
+            return 1;
+        }
+
+        for (let i = 0; i < within_text.length; i++) {
+            if (find_text === within_text[i]) {
+                return i + 1;
+            }
+        }
+        return "#VALUE!";
+    },
+
+    FINDB: (find_text, within_text) => {
+        find_text = H.accept(find_text, [Types.STRING]);
+        within_text = H.accept(within_text, [Types.STRING]);
+
+        if (find_text === "") {
+            return 1;
+        }
+
+        for (let i = 0; i < within_text.length; i++) {
+            if (find_text === within_text[i]) {
+                return i + 1;
+            }
+        }
+        return "#VALUE!";
+    },
+
+    FIXED: (number, decimals) => {
+        number = H.accept(number, [Types.NUMBER]);
+        //decimals = H.accept(decimals, [Types.NUMBER]);
+        let n = Math.pow(10, decimals);
+
+        if (!decimals) {
+            n = Math.pow(10, 2);
+        }
+
+        if (number < 0) {
+            number = Math.abs(number);
+            return Math.round(number * n) / n * (-1);
+        } else {
+            return Math.round(number * n) / n;
+        }
 
     },
 
-    EXACT: (...params) => {
+    LEFT: (text, num_chars) => {
+        text = H.accept(text, [Types.STRING]);
+        let str = "";
 
+        if (!num_chars) {
+            return text[0];
+        }
+
+        for (let i = 0; i < num_chars; i++){
+            str += text[i];
+        }
+        return str;
     },
 
-    FIND: (...params) => {
+    LEFTB: (text, num_bytes) => {
+        text = H.accept(text, [Types.STRING]);
+        let str = "";
 
+        if (!num_bytes) {
+            return text[0];
+        }
+
+        for (let i = 0; i < num_bytes; i++){
+            str += text[i];
+        }
+        return str;
     },
 
-    FINDB: (...params) => {
-
+    LEN: (text) => {
+        text = H.accept(text, [Types.STRING]);
+        return text.length;
     },
 
-    FIXED: (...params) => {
-
+    LENB: (text) => {
+        text = H.accept(text, [Types.STRING]);
+        return text.length;
     },
 
-    LEFT: (...params) => {
-
+    LOWER: (text) => {
+        text = H.accept(text, [Types.STRING]);
+        return text.toLowerCase();
     },
 
-    LEFTB: (...params) => {
+    MID: (text,start_num,num_chars) => {
+        text = H.accept(text, [Types.STRING]);
+        start_num = H.accept(start_num, [Types.NUMBER]);
+        num_chars = H.accept(num_chars, [Types.NUMBER]);
+        let str = "";
 
+        for (let i = start_num - 1; i < num_chars; i++){
+            str += text[i];
+        }
+        return str;
     },
 
-    LEN: (...params) => {
+    MIDB: (text,start_num,num_chars) => {
+        text = H.accept(text, [Types.STRING]);
+        start_num = H.accept(start_num, [Types.NUMBER]);
+        num_chars = H.accept(num_chars, [Types.NUMBER]);
+        let str = "";
 
+        for (let i = start_num - 1; i < num_chars; i++){
+            str += text[i];
+        }
+        return str;
     },
 
-    LENB: (...params) => {
-
-    },
-
-    LOWER: (...params) => {
-
-    },
-
-    MID: (...params) => {
-
-    },
-
-    MIDB: (...params) => {
-
-    },
-
-    NUMBERVALUE: (...params) => {
+    NUMBERVALUE: (text, decimal_separator, group_separator) => {
 
     },
 
@@ -88,28 +210,72 @@ const TextFunctions = {
 
     },
 
-    PROPER: (...params) => {
+    PROPER: (text) => {
+        text = H.accept(text, [Types.STRING]);
+        let str = text.split(" ");
+        const word = [];
+
+        for (let char of str){
+            word.push(char[0].toUpperCase() + char.slice(1));
+        }
+        return word.join(" ");
+    },
+
+    REPLACE: (old_text, start_num, num_chars, new_text) => {
+        old_text = H.accept(old_text, [Types.STRING]);
+        start_num = H.accept(start_num, [Types.NUMBER]);
+        num_chars = H.accept(num_chars, [Types.NUMBER]);
+        new_text = H.accept(new_text, [Types.STRING]);
+
+        let arr = old_text.split("");
+        arr.splice(start_num - 1, num_chars, new_text);
+
+        return arr.join("");
 
     },
 
-    REPLACE: (...params) => {
+    REPLACEB: (old_text, start_num, num_bytes, new_text) => {
+        old_text = H.accept(old_text, [Types.STRING]);
+        start_num = H.accept(start_num, [Types.NUMBER]);
+        num_bytes = H.accept(num_bytes, [Types.NUMBER]);
+        new_text = H.accept(new_text, [Types.STRING]);
+
+        let arr = old_text.split("");
+        arr.splice(start_num - 1, num_bytes, new_text);
+
+        return arr.join("");
+    },
+
+    REPT: (text, number_times) => {
 
     },
 
-    REPLACEB: (...params) => {
+    RIGHT: (text, num_chars) => {
+        text = H.accept(text, [Types.STRING]);
+        let str = "";
 
+        if (!num_chars) {
+            return text[text.length - 1];
+        }
+
+        for (let i = 0; i < num_chars; i++){
+            str = text[text.length - i - 1] + str;
+        }
+        return str;
     },
 
-    REPT: (...params) => {
+    RIGHTB: (text, num_bytes) => {
+        text = H.accept(text, [Types.STRING]);
+        let str = "";
 
-    },
+        if (!num_bytes) {
+            return text[text.length - 1];
+        }
 
-    RIGHT: (...params) => {
-
-    },
-
-    RIGHTB: (...params) => {
-
+        for (let i = 0; i < num_bytes; i++){
+            str = text[text.length - i - 1] + str;
+        }
+        return str;
     },
 
     SEARCH: (...params) => {
@@ -140,15 +306,19 @@ const TextFunctions = {
 
     },
 
-    TEXTJOIN: (...params) => {
-
-    },
-
     UNICHAR: (...params) => {
 
     },
 
     UNICODE: (...params) => {
+
+    },
+
+    UPPER: (...params) => {
+
+    },
+
+    VALUE: (...params) => {
 
     },
 };
--- test/formulas/text.js
+++ test/formulas/text.js
@@ -4,7 +4,118 @@ const TextFunctions = require('../../formulas/text');
 
 describe('Text Functions', function () {
     it('ASC should ..', function () {
-        const result = TextFunctions.ASC();
-        expect(result).to.equal(undefined)
+        const result = TextFunctions.ASC("ＡＢＣ");
+        expect(result).to.equal("ABC")
     });
+
+    it('BAHTTEXT should ..', function () {
+        const result = TextFunctions.BAHTTEXT("1234");
+        expect(result).to.equal("")
+    });
+
+    it('CHAR should ..', function () {
+        const result = TextFunctions.CHAR("67");
+        expect(result).to.equal("C")
+    });
+
+    it('CLEAN should ..', function () {
+        const result = TextFunctions.CLEAN("äÄçÇéÉêPHP-MySQLöÖÐþúÚ");
+        expect(result).to.equal("PHP-MySQL")
+    });
+
+    it('CODE should ..', function () {
+        const result = TextFunctions.CODE("C");
+        expect(result).to.equal(67)
+    });
+
+    it('CONCAT should ..', function () {
+        const result = TextFunctions.CONCAT("I"," hate"," carrot");
+        expect(result).to.equal("I hate carrot")
+    });
+
+    it('DOLLAR should ..', function () {
+        const result = TextFunctions.DOLLAR(1234567);
+        expect(result).to.equal("$1,234,567")
+    });
+
+    it('EXACT should ..', function () {
+        const result = TextFunctions.EXACT("hello", "Hello");
+        expect(result).to.equal(false)
+    });
+
+    it('FIND should ..', function () {
+        const result = TextFunctions.FIND("h", "Hello");
+        expect(result).to.equal("#VALUE!")
+    });
+
+    it('FINDB should ..', function () {
+        const result = TextFunctions.FINDB("o", "Hello");
+        expect(result).to.equal(5)
+    });
+
+    it('FIXED should ..', function () {
+        const result = TextFunctions.FIXED(1234.567, 1);
+        expect(result).to.equal(1234.6)
+    });
+
+    it('FIXED should ..', function () {
+        const result = TextFunctions.FIXED(1234.567, -1);
+        expect(result).to.equal(1230)
+    });
+
+    it('FIXED should ..', function () {
+        const result = TextFunctions.FIXED(-1234.567, -1);
+        expect(result).to.equal(-1230)
+    });
+
+    it('FIXED should ..', function () {
+        const result = TextFunctions.FIXED(44.332);
+        expect(result).to.equal(44.33)
+    });
+
+    it('LEFT should ..', function () {
+        const result = TextFunctions.LEFT("Salesman");
+        expect(result).to.equal("S")
+    });
+
+    it('LEFT should ..', function () {
+        const result = TextFunctions.LEFT("Salesman",4);
+        expect(result).to.equal("Sale")
+    });
+
+    it('RIGHT should ..', function () {
+        const result = TextFunctions.RIGHT("Salesman");
+        expect(result).to.equal("n")
+    });
+
+    it('RIGHT should ..', function () {
+        const result = TextFunctions.RIGHT("Salesman",3);
+        expect(result).to.equal("man")
+    });
+
+    it('LEN should ..', function () {
+        const result = TextFunctions.LEN("Phoenix, AZ");
+        expect(result).to.equal(11)
+    });
+
+    it('LOWER should ..', function () {
+        const result = TextFunctions.LOWER("E. E. Cummings");
+        expect(result).to.equal("e. e. cummings")
+    });
+
+    it('MID should ..', function () {
+        const result = TextFunctions.MID("Fluid Flow",1,5 );
+        expect(result).to.equal("Fluid")
+    });
+
+    it('PROPER should ..', function () {
+        const result = TextFunctions.PROPER("this is a tiTle");
+        expect(result).to.equal("This Is A TiTle")
+    });
+
+    it('REPLACE should ..', function () {
+        const result = TextFunctions.REPLACE("abcdefghijk", 6, 5, "*");
+        expect(result).to.equal("abcde*k")
+    });
+
 });
